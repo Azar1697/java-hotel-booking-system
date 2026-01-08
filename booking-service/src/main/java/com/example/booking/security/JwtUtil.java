4b34bdf785162868d7e5a032779a877fab505828
Azar1697 <127751837+Azar1697@users.noreply.github.com>
2026-01-08 09:49:30 +0300

    Update JwtUtil.java

--- booking-service/src/main/java/com/example/booking/security/JwtUtil.java
+++ booking-service/src/main/java/com/example/booking/security/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    // Секретный ключ (должен быть длинным и сложным!)
+    // Секретный ключ 
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
     public String generateToken(String userName) {
